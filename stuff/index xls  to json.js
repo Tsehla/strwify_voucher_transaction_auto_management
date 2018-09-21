3fa545dfc059d3e31d9bcb12f798f84bf604dbe9
Tsehla Nkhi <petrusnk@gmail.com>
2018-09-21 05:36:20 -0400

    Add files via upload

---
+++ stuff/index xls  to json.js
@@ -0,0 +1,14 @@
+
+
+
+                let XLSX = require('xlsx');
+                
+                let excel_file = XLSX.readFile('./test.xls');
+                
+                //user default sheet name [Sheet1/ Sheet2/ sheet3/etc]
+                //use one sheet per excel work book
+       
+                let exlcel_contents_array = XLSX.utils.sheet_to_json(excel_file.Sheets.Sheet1);
+     
+              	 exlcel_contents_array.forEach(function(item, n) { });
+                
