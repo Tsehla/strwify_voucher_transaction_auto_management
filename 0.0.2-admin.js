e73bc81e02a843cd6fe6dca1c4653aa56b55ee31
Tsehla Nkhi <petrusnk@gmail.com>
2018-09-21 05:26:16 -0400

    Add files via upload

---
+++ 0.0.2-admin.js
@@ -0,0 +1,212 @@
+exports.create={
+    
+    'server admin' : [{
+        name : 'tsehla',
+        idnumber : 8905135852087,
+        contact : 719010014,
+        password : '12345asdf',
+        email : 'tsehla@streetwifiy.co.za'
+        
+    }],
+    
+    'Voucher Codes' : [
+        {
+        vouchercode :'123asdf' ,
+        voucherstate : 'new',
+        },
+        {
+        vouchercode : '456asdf',
+        voucherstate : 'new',
+        },
+        {
+        vouchercode : '789asdf',
+        voucherstate : 'new',
+        }, 
+        {
+        vouchercode :'101asdf' ,
+        voucherstate : 'new',
+        }, 
+        {
+        vouchercode : '121asdf',
+        voucherstate : 'new',
+        }, 
+        {
+        vouchercode : '141asdf',
+        voucherstate : 'new',
+        }, 
+        {
+        vouchercode : '151asdf',
+        voucherstate : 'new',
+        }, 
+        {
+        vouchercode : '161asdf',
+        voucherstate : 'new',
+        }, 
+        {
+        vouchercode : '171asdf',
+        voucherstate : 'new',
+        }, 
+        {
+        vouchercode : '181asdf',
+        voucherstate : 'new',
+        }, 
+        {
+        vouchercode : '191asdf',
+        voucherstate : 'new',
+        }, 
+        {
+        vouchercode : '122asdf',
+        voucherstate : 'new',
+        }, 
+        {
+        vouchercode : '132asdf',
+        voucherstate : 'new',
+        }, 
+        {
+        vouchercode : '142asdf',
+        voucherstate : 'new',
+        }, 
+        {
+        vouchercode : '152asdf',
+        voucherstate : 'new',
+        } ],
+    'seller distributor' : [
+        {
+        name : 'dist_1_name',
+        surname : 'dist_1_lastname',
+        idnumber : '1234567891234',
+        contact : ['071000000', '081000000', '091111111'],
+        address : '7184 ext 4 JHB _1',
+        city : 'jhb_1',
+        province : 'gauteng_1',
+        password : 'password1',
+        email : 'dist_1@streetwifiy.co.za',
+        credit : 100,
+        usertype : 'Distributor'
+        
+        },        
+        {
+        name : 'dist_2_name',
+        surname : 'dist_2_lastname',
+        idnumber : '1234567891234',
+        contact : ['071000000', '081000000', '091111111'],
+        address : '7184 ext 4 JHB _1',
+        city : 'jhb_2',
+        province : 'gauteng_2',
+        password : 'password1',
+        email : 'dist_2@streetwifiy.co.za',
+        credit : 100,
+        usertype : 'Distributor'
+        
+        },        {
+        name : 'dist_3_name',
+        surname : 'dist_3_lastname',
+        idnumber : '1234567891234',
+        contact : ['071000000', '081000000', '091111111'],
+        address : '7184 ext 4 JHB _3',
+        city : 'jhb_3',
+        province : 'gauteng_3',
+        password : 'password3',
+        email : 'dist_3@streetwifiy.co.za',
+        credit : 100,
+        usertype : 'Distributor'
+        
+        },        {
+        name : 'dist_4_name',
+        surname : 'dist_4_lastname',
+        idnumber : '1234567891234',
+        contact : ['071000000', '081000000', '091111111'],
+        address : '7184 ext 4 JHB _4',
+        city : 'jhb_4',
+        province : 'gauteng_4',
+        password : 'password4',
+        email : 'dist_4@streetwifiy.co.za',
+        credit : 100,
+        usertype : 'Distributor'
+        
+        },       {
+        name : 'dist_5_name',
+        surname : 'dist_5_lastname',
+        idnumber : '1234567891234',
+        contact : ['071000000', '081000000', '091111111'],
+        address : '7184 ext 4 JHB _5',
+        city : 'jhb_5',
+        province : 'gauteng_5',
+        password : 'password5',
+        email : 'dist_1@streetwifiy.co.za',
+        credit : 100,
+        usertype : 'Distributor'
+        
+        },      
+        {
+        name : 'seller_1_name',
+        surname : 'seller_1_lastname',
+        idnumber : '1234567891234',
+        contact : ['071000000', '081000000', '091111111'],
+        address : '7184 ext 4 JHB _1',
+        city : 'jhb_1',
+        province : 'gauteng_1',
+        password : 'password1',
+        email : 'seller_1@streetwifiy.co.za',
+        credit : 100,
+        usertype : 'Seller'
+        
+        },
+                    {
+        name : 'seller_2_name',
+        surname : 'seller_2_lastname',
+        idnumber : '1234567891234',
+        contact : ['071000000', '081000000', '091111111'],
+        address : '7184 ext 4 JHB _2',
+        city : 'jhb_2',
+        province : 'gauteng_2',
+        password : 'password2',
+        email : 'seller_1@streetwifiy.co.za',
+        credit : 10,
+        usertype : 'Seller'
+        
+        },
+                    {
+        name : 'seller_3_name',
+        surname : 'seller_3_lastname',
+        idnumber : '1234567891234',
+        contact : ['071000000', '081000000', '091111111'],
+        address : '7184 ext 4 JHB _3',
+        city : 'jhb_3',
+        province : 'gauteng_3',
+        password : 'password3',
+        email : 'seller_3@streetwifiy.co.za',
+        credit : 50,
+        usertype : 'Seller'
+        
+        },
+                    {
+        name : 'seller_4_name',
+        surname : 'seller_4_lastname',
+        idnumber : '1234567891234',
+        contact : ['071000000', '081000000', '091111111'],
+        address : '7184 ext 4 JHB _4',
+        city : 'jhb_4',
+        province : 'gauteng_4',
+        password : 'password4',
+        email : 'seller_1@streetwifiy.co.za',
+        credit : 100,
+        usertype : 'Seller'
+        
+        },
+                    {
+        name : 'seller_5_name',
+        surname : 'seller_5_lastname',
+        idnumber : '1234567891234',
+        contact : ['071000000', '081000000', '091111111'],
+        address : '7184 ext 4 JHB _5',
+        city : 'jhb_5',
+        province : 'gauteng_5',
+        password : 'password5',
+        email : 'seller_5@streetwifiy.co.za',
+        credit : 100,
+        usertype : 'Seller'
+        
+        } ]
+    
+};
