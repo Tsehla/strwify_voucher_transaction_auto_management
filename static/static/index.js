fdc4e2a3a2ed0255aa18c1f8664a86cc68c8c9ba
Tsehla Nkhi <petrusnk@gmail.com>
2019-01-18 15:24:16 +0200

    Add files via upload
    continuing

--- static/static/index.js
+++ static/static/index.js
@@ -248,7 +248,7 @@ function manual_voucher_init(){
                if(response != 'Voucher Not found'){
 
 				dom_innerHtml('second_page_user_auto_code', 'Voucher Ready');
-               var show_code = "<p style='color:green;margin:0px;padding:0px;height:0px;width:0px'>Please Enter This Voucher Code : <span style='color:red; margin:0px;padding:0px;height:auto;width:auto'>"+response+"</span></p>";
+               var show_code = "<p style='color:green;margin:0px;padding:0px;height:0px;width:0px'>Please Enter This Voucher Code : <span style='color:red; margin:0px;padding:0px;height:auto;width:auto'>"+JSON.stringify(response.vouchercode)+"</span></p>";
                dom_innerHtml('second_page_ticket_status', show_code);
                voucher_print(response);//print voucher
                }
