d80b203543b041c5496faec1d05bd1a52e51c45b
tsehlan <tsehlankhi@gmail.com>
2019-12-02 14:25:49 +0200

    _

---
+++ updates/0.0.3-admin.js
@@ -0,0 +1,175 @@
+exports.create={
+    
+    'server admin' : [{
+        name : 'tsehla',
+        idnumber : 8905135800000,
+        contact : 719010014,
+        password : '12345asdf',
+        email : 'tsehla@streetwifiy.co.za'
+        
+    }],
+    
+    'Voucher Codes' : [
+        {
+        voucheramount : 1,
+        vouchercode :'123asdf',
+        voucherstate : 'new',
+	
+        },
+        {//free voucher format
+            voucheramount : 0,
+            voucher_username : 'free',
+            voucher_password : 'voucher',
+            voucherstate : 'new',
+            voucherprofile : '20 MB',
+        
+        },
+        {
+        voucheramount : 1,
+        vouchercode : '456asdf',
+        voucherstate : 'new',
+		voucherprofile : '150 MB',
+		voucherexpiry : '2 DAYS'
+        },
+        {
+        vouchercode : '789asdf',
+        voucherstate : 'new',
+		voucherprofile : '150 MB',
+		voucherexpiry : '2 DAYS'
+        }, 
+        {
+        voucheramount : 1,
+        vouchercode :'101asdf' ,
+        voucherstate : 'new',
+		voucherprofile : '150 MB',
+		voucherexpiry : '2 DAYS'
+        }, 
+        {
+        voucheramount : 1,
+        vouchercode : '121asdf',
+        voucherstate : 'new',
+		voucherprofile : '150 MB',
+		voucherexpiry : '2 DAYS'
+        }, 
+        {
+        voucheramount : 1,
+        vouchercode : '141asdf',
+        voucherstate : 'new',
+		voucherprofile : '150 MB',
+		voucherexpiry : '2 DAYS'
+        }, 
+        {
+        voucheramount : 1,
+        vouchercode : '151asdf',
+        voucherstate : 'new',
+		voucherprofile : '150 MB',
+		voucherexpiry : '2 DAYS'
+        }, 
+        {
+        voucheramount : 1,
+        vouchercode : '161asdf',
+        voucherstate : 'new',
+		voucherprofile : '150 MB',
+		voucherexpiry : '2 DAYS'
+        }, 
+        {
+        voucheramount : 1,
+        vouchercode : '171asdf',
+        voucherstate : 'new',
+		voucherprofile : '150 MB',
+		voucherexpiry : '2 DAYS'
+        }, 
+        {
+        voucheramount : 2,
+        vouchercode : '181asdf',
+        voucherstate : 'new',
+        voucherprofile : '200 MB',
+		voucherexpiry : '2 DAYS'
+        }, 
+        {
+        voucheramount : 2,
+        vouchercode : '191asdf',
+        voucherstate : 'new',
+        voucherprofile : '200 MB',
+		voucherexpiry : '2 DAYS'
+        }, 
+        {
+        voucheramount : 1,
+        vouchercode : '122asdf',
+        voucherstate : 'new',
+        voucherprofile : '200 MB',
+		voucherexpiry : '2 DAYS'
+        }, 
+        {
+        voucheramount : 2,
+        vouchercode : '132asdf',
+        voucherstate : 'new',
+        voucherprofile : '200 MB',
+		voucherexpiry : '2 DAYS'
+        }, 
+        {
+        voucheramount : 2,
+        vouchercode : '142asdf',
+        voucherstate : 'new',
+        voucherprofile : '200 MB',
+		voucherexpiry : '2 DAYS'
+        }, 
+        {
+        voucheramount : 2,
+        vouchercode : '152asdf',
+        voucherstate : 'new',
+        voucherprofile : '200 MB',
+		voucherexpiry : '2 DAYS'
+        } ],
+    'seller distributor' : [{
+		name : 'Tsehla',
+        surname : 'Admin',
+        idnumber : '8905135800000',
+        contact : ['071000000', '081000000', '091111111'],
+        address : '7184 ext 4 JHB _2',
+        city : 'jhb_2',
+        province : 'gauteng_2',
+        password : '12345asdf',
+        email : 'tsehla@streetwifiy.co.za',
+        credits : 500,
+        usertype : 'Server Admin'
+        
+        },
+        {
+            name : 'Tsehla',
+            surname : 'Seller',
+            idnumber : '8905135800000',
+            contact : ['071000000', '081000000', '091111111'],
+            address : '7184 ext 4 JHB _2',
+            city : 'jhb_2',
+            province : 'gauteng_2',
+            password : '12345asdf',
+            email : 'tsehla@streetwifiy.co.za',
+            credits : 500,
+            usertype : 'Seller'
+            
+            },
+            {
+                name : 'Tsehla',
+                surname : 'Distributor',
+                idnumber : '8905135800000',
+                contact : ['071000000', '081000000', '091111111'],
+                address : '7184 ext 4 JHB _2',
+                city : 'jhb_2',
+                province : 'gauteng_2',
+                password : '12345asdf',
+                email : 'tsehla@streetwifiy.co.za',
+                credits : 500,
+                usertype : 'Distributor'
+                
+                }
+    
+
+	],
+	
+		'Router Monitoring':[
+			{routername : 'Orange farm mall', routerlocation:'Orange farm', routerdetails:'Contact tsehla if issue on 0719000000'},
+			{routername : 'Home', routerlocation:'Orange farm', routerdetails:'My home router'}
+		]
+    
+};
