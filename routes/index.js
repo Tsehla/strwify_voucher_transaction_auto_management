94de0c9eea7b30cdbbd3e988e683db6c6eab7324
tsehlan <tsehlankhi@gmail.com>
2019-12-07 10:27:30 +0200

    mib

--- routes/index.js
+++ routes/index.js
@@ -2090,9 +2090,39 @@ app.get('/api/hotspot_data', function(req, res){
 	
 	
 	
+	//================================
+	//			upload
+	//================================
+
+	//Images for backgrounds
+
+	app.post('/upload', function(req, res) {
+
+		fs.readFile(req.files.image.path, function (err, data) {
 	
+			var imageName = req.files.image.name
 	
+			/// If there's an error
+			if(!imageName){
 	
+				console.log("There was an error")
+				res.redirect("/");
+				res.end();
+	
+			} else {
+	
+			  var newPath = __dirname + "/uploads/fullsize/" + imageName;
+	
+			  /// write file to uploads/fullsize folder
+			  fs.writeFile(newPath, data, function (err) {
+	
+				/// let's see it
+				res.redirect("/uploads/fullsize/" + imageName);
+	
+			  });
+			}
+		});
+	});
 	
 	
 	
