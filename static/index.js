5f470be9a6a02ea1ac9e05c9892a814d0e24d5a4
tsehlan <tsehlankhi@gmail.com>
2019-12-12 23:10:54 +0200

    mib

--- static/index.js
+++ static/index.js
@@ -4716,7 +4716,7 @@ function qr_code_fn(uniqueCode){
 
 
 				container = document.getElementById('qr_container').innerHTML+=`	
-					<div class="imgblock" style='margin: 10px 0;text-align: center;float: center;min-height: 240px;height:50vh;border-bottom: 1px solid #B4B7B4;'>	
+					<div class="imgblock" style='margin: 10px 0;text-align: center;float: center;min-height: 320px;height:55vh;border-bottom: 1px solid #B4B7B4;'>	
 						<div class="qr" id="qrcode_div"></div>	
 			
 					</div>	
