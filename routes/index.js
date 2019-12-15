a57adedf724a416f0ed9f49b77b480ed0952501b
tsehlan <tsehlankhi@gmail.com>
2019-12-15 18:16:15 +0200

    refracture

--- routes/index.js
+++ routes/index.js
@@ -10,7 +10,8 @@ keystone.get('routes', function(app){
    // app.use(express.static('.//'));
 				
 	//	if(req.hostname.search('127.0.0.1') > -1 || req.hostname.search('localhost') > -1 || req.get('X-Forwarded-Proto')=='https' || req.protocol == 'https' ){
-					  
+	
+	app.options('*', function (req,res) { res.sendStatus(200); });
    //enable cors 
 	app.use(function(req, res, next) { //allow cross origin requests
           res.setHeader("Access-Control-Allow-Methods", "POST, PUT, OPTIONS, DELETE, GET");
