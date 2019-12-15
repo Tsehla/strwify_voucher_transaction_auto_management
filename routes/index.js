aa92afa0ba001f1544f6658cf9f1be6ed2a890a3
tsehlan <tsehlankhi@gmail.com>
2019-12-15 10:01:24 +0200

    refracture

--- routes/index.js
+++ routes/index.js
@@ -30,16 +30,23 @@ app.use(function(req, res, next) { //force https use
 
 
 		//Heroku stores the origin protocol in a header variable. The app itself is isolated within the dyno and all request objects have an HTTP protocol.
-		if (req.get('X-Forwarded-Proto')=='https' || req.hostname == 'localhost') {
+		if (req.get('X-Forwarded-Proto')=='https' || req.protocol == 'https' || req.hostname == 'localhost' || req.hostname == '127.0.0.1') {
 			//Serve  App by passing control to the next middleware
 			next();
 		}
 		
-		else if(req.get('X-Forwarded-Proto')!='https' && req.get('X-Forwarded-Port')!='443'){
+		else{
 			//Redirect if not HTTP with original request URL
 			res.redirect('https://' + req.hostname + req.url);
 		}
 
+		/*
+				
+		else if(req.get('X-Forwarded-Proto')!='https' && req.get('X-Forwarded-Port')!='443'){
+			//Redirect if not HTTP with original request URL
+			res.redirect('https://' + req.hostname + req.url);
+		}
+		*/
   });	
     
    //enable cors 
