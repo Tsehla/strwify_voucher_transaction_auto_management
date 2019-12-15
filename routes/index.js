75feede72b512d2bd4420cac9bc3c88ce29dca6a
tsehlan <tsehlankhi@gmail.com>
2019-12-15 10:30:41 +0200

    refracture

--- routes/index.js
+++ routes/index.js
@@ -8,7 +8,7 @@ var fs = require('fs'); //file read
 
 keystone.get('routes', function(app){
    // app.use(express.static('.//'));
-
+/*
 app.use(function(req, res, next) { //force https use
 		//console.log(req.secure);
 
@@ -54,9 +54,24 @@ app.use(function(req, res, next) { //force https use
 			//Redirect if not HTTP with original request URL
 			res.redirect('https://' + req.hostname + req.url);
 		}
-		*/
+		*
   });	
-    
+
+  */
+
+
+ function ensureSecure(req, res, next) {
+    //Heroku stores the origin protocol in a header variable. The app itself is isolated within the dyno and all request objects have an HTTP protocol.
+    if (req.get('X-Forwarded-Proto')=='https' || req.hostname == 'localhost') {
+        //Serve Angular App by passing control to the next middleware
+        next();
+    } else if(req.get('X-Forwarded-Proto')!='https' && req.get('X-Forwarded-Port')!='443'){
+        //Redirect if not HTTP with original request URL
+        res.redirect('https://' + req.hostname + req.url);
+    }
+}
+	
+app.use(ensureSecure);
    //enable cors 
 	app.use(function(req, res, next) { //allow cross origin requests
           res.setHeader("Access-Control-Allow-Methods", "POST, PUT, OPTIONS, DELETE, GET");
