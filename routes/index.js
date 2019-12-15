92e315cf5d27376a0a402b25856b205a81ecf814
tsehlan <tsehlankhi@gmail.com>
2019-12-15 10:11:16 +0200

    refracture

--- routes/index.js
+++ routes/index.js
@@ -12,20 +12,26 @@ keystone.get('routes', function(app){
 app.use(function(req, res, next) { //force https use
 		//console.log(req.secure);
 
-		/* NONE HEROKU HOSTING ssl checking
+		// NONE HEROKU HOSTING ssl checking
 			if(!req.secure){
 				
-				if(req.hostname.search('127.0.0.1') > -1 || req.hostname.search('localhost') > -1 ){}//ignore if local server
+				if(req.hostname.search('127.0.0.1') > -1 || req.hostname.search('localhost') > -1 || req.get('X-Forwarded-Proto')=='https' || req.protocol == 'https' ){
+					return next();
+					
+				}//ignore if local server
+
+				//else if(req.get('X-Forwarded-Proto')=='http' || req.protocol == 'http'){//change link to https/none local server
 
 				else{//change link to https/none local server
-					//console.log(req.headers)
-					res.redirect('https://'+ req.headers.host + req.url);
-					res.end;
-					return;
+						//console.log(req.headers)
+						res.redirect('https://'+ req.headers.host + req.url);
+						res.end;
+						return;
 				}
 			}
-		*/
+		
 
+		/*
 		//heroku app hosted ssl checking
 
 
