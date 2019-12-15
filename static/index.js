70980d53a01673526a7bc6a98374cef9fc717c61
tsehlan <tsehlankhi@gmail.com>
2019-12-15 17:22:36 +0200

    refracture

--- static/index.js
+++ static/index.js
@@ -18,7 +18,7 @@ var current_domain = window.location.host;//domain en port//use this on live
 
 
 //change https to http when on localhost
-var http_https = "https://";
+var http_https = "http://";
 
 if(current_domain.search('127.0.0.1') > -1 || current_domain.search('localhost') > -1){//match exist
 
